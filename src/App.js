15f389064a9b91a25cff0ae37352992f4297e625
MaduFalavigna <104871852+MaduFalavigna@users.noreply.github.com>
2022-09-14 19:42:32 -0300

    edição tela login

--- src/App.js
+++ src/App.js
@@ -4,11 +4,32 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src="/imagens/logocascavel.png" className="App-logo" alt="logo" />
         <p>
-          texto
+         LOGIN
         </p>
       </header>
-    </div>
+      <form className='App-titulo'>
+          <label>
+            Usuario
+            <p>
+            <input type="email" name="name" className='usuario' />
+            </p>
+          </label>
+         
+        </form> 
+        <form className='App-senha'>
+        <label>
+            Senha
+            <p>
+            <input type="password" name="name" className='senha' />
+            </p>
+          </label>
+          </form> 
+          <input type="submit" value="Entrar" />
+          </div> 
+          
+          
   );
 }
 
