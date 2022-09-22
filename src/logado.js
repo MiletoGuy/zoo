b106f0ee3e2dd836bb353ad6e906efe8b51f3658
MaduFalavigna <104871852+MaduFalavigna@users.noreply.github.com>
2022-09-21 21:37:38 -0300

    21/09/22
    .

---
+++ src/logado.js
@@ -0,0 +1,27 @@
+import './logado.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/imagens/logocascavel.png" className="App-logo" alt="logo" />
+      </header>
+      <div className="logoservicos">
+        <input className="logoservicos" type="image" src="/imagens/logozoo.png" height="250" width="250"/>
+        <input className="logoservicos" type="image" src="/imagens/logosamucao.png" height="250" width="250"/>
+        <input className="logoservicos" type="image" src="/imagens/logocastramovel.png" height="250" width="250"/> 
+      </div>
+      <div className="textname">
+        <a className="title" href="/zoo.js">ZOOLÓGICO</a>
+        <a className="title" href="/samucao">SAMUCÃO</a>
+        <a className="title" href="castramovel">CASTRAMÓVEL</a>
+      
+      
+      </div>
+    </div> 
+          
+          
+  );
+}
+
+export default App;
