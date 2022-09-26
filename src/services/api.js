6238115cc773ee1485fbd677aab5739840c65bda
Tales <tales5c@outlook.com>
2022-09-26 14:15:54 -0300

    Merge filtro

---
+++ src/services/api.js
@@ -0,0 +1,15 @@
+import './Api.css';
+
+function Api() {
+  return (
+    <div className="Api">
+      <header className="Api-header">
+        <p>
+          API
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Api;
