d060f37583996b08e94811b41890329b10aad425
MaduFalavigna <67002662+mariaeduardafalavigna@users.noreply.github.com>
2022-10-17 21:36:32 -0300

    17/10/22

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './Filtro';
+import App from './Enriquecimento';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
