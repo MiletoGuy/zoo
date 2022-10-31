2c4b30b4b5d1786069168daa7f21ddb4f2c624d6
CiroRN <87498222+CiroRN@users.noreply.github.com>
2022-10-31 20:14:04 -0300

    cadastro animal
    n sei oq precisa ter caso do figma intao fiz issae, caso precise mudar só avisar

---
+++ src/Cadastroanimal.js
@@ -0,0 +1,78 @@
+import { useState } from "react"
+import './Cadastroanimal.css';
+
+function App() {
+
+  return (
+    <div className='app'>
+
+      <header className="logo">
+        <img src="/imagens/logozoocasc.jpg" className="img-logo" />
+        <div className="conta">
+        <button className="email">email</button>
+        </div>
+      </header>
+      <header className="barra">
+        <button className="consultani">Cadastrar Animal</button>
+      </header>
+      <header className="titulo">
+        <t1 className="consultaranimal">
+          Cadastrar Animal
+        </t1>
+        <p></p>
+        <t2 className="digite">
+          Preencha os campos de acordo com as informações do animal
+        </t2>
+      </header>
+
+      <div className="barrapesquisa">
+        <div className="bal">
+          <label className="lab">Apelido</label>
+          <input type="text" placeholder='...' className='search' />
+        </div>
+        <div className="bal">
+          <label className="lab">Idade</label>
+          <input type="text" placeholder='...' className='search' />
+        </div>
+        <div className="bal">
+          <label className="lab">Sexo</label>
+          <input type="text" placeholder='...' className='search' />
+        </div>
+      </div>
+
+      <div className="barrapesquisa">
+        <div className="bal">
+          <label className="lab">Identificação</label>
+          <input type="text" placeholder='...' className='search' />
+        </div>
+        <div className="bal">
+          <label className="lab">Codigo</label>
+          <input type="text" placeholder='...' className='search' />
+        </div>
+        <div className="bal">
+          <label className="lab">Habitat</label>
+          <input type="text" placeholder='...' className='search' />
+        </div>
+      </div>
+
+      <div className="barrapesquisa">
+        <div className="bal">
+          <label className="lab">Especie</label>
+          <input type="text" placeholder='...' className='search' />
+        </div>
+        <div className="bal">
+          <label className="lab">Status</label>
+          <input type="text" placeholder='...' className='search' />
+        </div>
+        <div className="bal">
+          <label className="lab">Data de entrada</label>
+          <input type="text" placeholder='...' className='search' />
+        </div>
+      </div>
+      
+      <div className="add"><button className="adicionar">Cadastrar</button></div>
+    </div>
+  );
+}
+
+export default App;
