993576e089b7d669d91bda54a2898c75c947cdea
MaduFalavigna <67002662+mariaeduardafalavigna@users.noreply.github.com>
2022-11-01 21:08:59 -0300

    cadastro novos

--- src/Cadastroanimal.js
+++ src/Cadastroanimal.js
@@ -1,7 +1,30 @@
 import { useState } from "react"
+import Modal from 'react-modal';
 import './Cadastroanimal.css';
 
+Modal.setAppElement('#root')
+
 function App() {
+  const [modalIsOpen, setIsOpen] = useState(false)
+
+  function OpenModal() {
+    setIsOpen(true)
+  }
+
+   function CloseModal() {
+    setIsOpen(false)
+   }
+
+   const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)'
+    }
+   }
 
   return (
     <div className='app'>
@@ -17,11 +40,11 @@ function App() {
       </header>
       <header className="titulo">
         <t1 className="consultaranimal">
-          Cadastrar Animal
+          Cadastrar Animal 
         </t1>
         <p></p>
         <t2 className="digite">
-          Preencha os campos de acordo com as informações do animal
+        Preencha os campos de acordo com as informações do animal
         </t2>
       </header>
 
@@ -69,9 +92,30 @@ function App() {
           <input type="text" placeholder='...' className='search' />
         </div>
       </div>
-      
       <div className="add"><button className="adicionar">Cadastrar</button></div>
+      
+      <div className="cadastro">
+        <button className="OpenModal">Cadastro espécie e identificação</button>
+        <modal
+        isOPen={modalIsOpen}
+        onRequestClose={CloseModal}
+        contentLabel="Exemple modal"
+        overLayClassName="modal-overlay"
+        className="modal-content"
+        style={customStyles}
+        ><h2>Cadastro de nova espécie!</h2>
+          <button onClick={CloseModal}>x</button>
+          <form action="">
+            <input type="text" placeholder="Espécie" />
+            <input type="text" placeholder="Nome Cientifico" />
+            <p></p>
+            <input type="text" placeholder="Tipo identificação" />
+          </form>
+        </modal>
+      </div>
+   
     </div>
+    
   );
 }
 
