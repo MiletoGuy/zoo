2f649d7f3f4216d12fff940b106867229c9581e7
CiroRN <87498222+CiroRN@users.noreply.github.com>
2022-11-26 20:04:27 -0300

    perfil 2
    Eletric boogaloo

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,7 @@ import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Filtro from "../pages/Filtro";
 import Perfil from "../pages/Perfil";
+import Perfil2 from "../pages/Perfil2";
 import HistoricoEtimologico from "../pages/HistoricoEtimologico";
 import AutServMed from "../pages/AutServMed";
 import CadastroMunicipal from "../pages/CadastroMunicipal";
@@ -20,6 +21,7 @@ const Rotas = () => {
                 <Route element = { <AutServMed/> } path="/autservmed"/>
                 <Route element = { <CadastroMunicipal/> } path="/cadastromunicipal"/>
                 <Route element = { <Perfil/> } path="/perfil"/>
+                <Route element = { <Perfil2/> } path="/perfil2"/>
             </Routes>
         </BrowserRouter>
     )
